8bef80635e522694f3af46e70174bae3ee17a8b0
Drake <zudrake.yh@gmail.com>
2020-03-17 14:30:29 -0700

    Components-2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
+import { Navbar, NavbarBrand, Form } from "reactstrap";
 import Directory from "./components/DirectoryComponent";
 import "./App.css";
-
+import { CAMPSITES } from "./shared/campsites";
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      campsites: CAMPSITES
+    };
+  }
   render() {
     return (
       <div className="App">
@@ -12,7 +18,7 @@ class App extends Component {
             <NavbarBrand href="/">NuCamp</NavbarBrand>
           </div>
         </Navbar>
-        <Directory />
+        <Directory campsites={this.state.campsites} />
       </div>
     );
   }
