cbc3fe525ab9d871ad6e57c1e1f85e56ebcbc6e6
Drake <zudrake.yh@gmail.com>
2020-03-23 13:43:24 -0700

    Presentational and Container Components

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -40,7 +40,7 @@ class Directory extends Component {
     const directory = this.props.campsites.map(campsite => {
       return (
         <div key={campsite.id} className="col-md-5 m-1">
-          <Card onClick={() => this.onCampsiteSelect(campsite)}>
+          <Card onClick={() => this.props.onClick(campsite.id)}>
             <CardImg width="100%" src={campsite.image} alt={campsite.name} />
             <CardImgOverlay>
               <CardTitle>{campsite.name}</CardTitle>
@@ -52,9 +52,9 @@ class Directory extends Component {
 
     return (
       <div className="container">
-        <div className="row">{directory}</div>
-
-        <CampsiteInfo campsite={this.state.selectedCampsite} />
+        <div className="row">
+          {directory}
+        </div>
       </div>
     );
   }
