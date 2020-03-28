216fc0f8166cbbbf102b451192029d076676a78f
Drake <zudrake.yh@gmail.com>
2020-03-27 21:46:48 -0700

    Adding to the Single Page App

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
+function RenderCard({ item }) {
+    return (
+        <Card>
+            <CardImg src={item.image} alt={item.name} />
+            <CardBody>
+                <CardTitle>{item.name}</CardTitle>
+                <CardText>{item.description}</CardText>
+            </CardBody>
+        </Card>
+    );
+}
 function Home(props) {
     return (
         <div className="container">
