825270cc6a6613ec8494f66b08ef27c19d94c32a
Drake <zudrake.yh@gmail.com>
2020-04-06 12:46:19 -0700

    CampsiteInfoComponent Validation

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, Button, Breadcrumb, BreadcrumbItem, Modal, ModalHeader, Label, Row, Col, ModalBody } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Component } from 'react';
 
 function RenderComments({ comments }) {
   if (comments) {
@@ -23,6 +25,7 @@ function RenderComments({ comments }) {
             </div>
           );
         })}
+        <CommentForm />
       </div>
     );
   }
@@ -40,7 +43,94 @@ function RenderCampsite({ campsite }) {
     </div>
   );
 }
+const required = val => val && val.length;
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
 
+class CommentForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isModalOpen: false,
+      rating: '',
+      author: '',
+      comment: '',
+      touched: {
+        rating: false,
+        author: false,
+        comment: false,
+      }
+    }
+    this.toggleModal = this.toggleModal.bind(this);
+  }
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
+    });
+  }
+
+  valueCheck(values) {
+
+    // console.log(values);
+    alert(JSON.stringify(values));
+  }
+
+  render() {
+
+    return (
+      <div>
+        <Button outline color="primary" onClick={this.toggleModal}><i className="fa fa-pencil fa-lg" />Submit Comment</Button>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader> Submit Comment </ModalHeader>
+          <ModalBody>
+            <LocalForm onSubmit={values => this.valueCheck(values)}>
+              <Row className="form-group">
+                <Col>
+                  <Label >Rating</Label>
+                  <Control.select className="form-control" model='.rating' id='rating' >
+                    <option value="1" >1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                  </Control.select>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Label>Author</Label>
+                  <Control.text model='.author' className="form-control" id="Author" validators={{ required, minLength: minLength(2), maxLength: maxLength(15) }}></Control.text>
+                  <Errors
+                    className="text-danger"
+                    model=".author"
+                    show="touched"
+
+                    messages={{
+                      required: 'required',
+                      minLength: 'Must be at least 2 characters',
+                      maxLength: 'Must be 15 characters or less'
+                    }}
+                  />
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Label>Text</Label>
+                  <Control.textarea model='.text' className="form-control" id="Text"></Control.textarea>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Button color="primary" model="./submit" type='submit' value='submit' validators={{ required, minLength: minLength(2), maxLength: maxLength(15) }} >Submit</Button>
+                </Col>
+              </Row>
+            </LocalForm>
+          </ModalBody>
+        </Modal>
+      </div >
+    );
+  }
+}
 function CampsiteInfo(props) {
   if (props.campsite) {
     return (
